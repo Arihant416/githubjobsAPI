7dd08a93969ce0fff0fb72ee337e63508ea503a0
Arihant416 <arihantjain416@gmail.com>
2021-03-10 16:35:53 +0530

    CORS-Resolved

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -5,10 +5,9 @@ const ACTIONS = {
 	MAKE_REQUEST: 'make-request',
 	GET_DATA: 'get-data',
 	ERROR: 'error',
-	UPDATE_NEXT_PAGE: 'update-next-page',
+	UPDATE_NEXT_PAGE: 'update-next-page'
 };
-const baseURL =
-	'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
+const baseURL = 'https://sheltered-savannah-57062.herokuapp.com/https://jobs.github.com/positions.json';
 function reducer(state, action) {
 	switch (action.type) {
 		case ACTIONS.MAKE_REQUEST:
@@ -20,7 +19,7 @@ function reducer(state, action) {
 				...state,
 				loading: false,
 				error: action.payload.error,
-				jobs: [],
+				jobs: []
 			};
 		case ACTIONS.UPDATE_NEXT_PAGE:
 			return { ...state, hasNextPage: action.payload.hasNextPage };
@@ -29,43 +28,46 @@ function reducer(state, action) {
 	}
 }
 export default function useFetchJobs(params, page) {
-	const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
-	useEffect(() => {
-		const cancelToken1 = axios.CancelToken.source();
-		dispatch({ type: ACTIONS.MAKE_REQUEST });
-		axios
-			.get(baseURL, {
-				cancelToken: cancelToken1.token,
-				params: { markdown: true, page: page, ...params },
-			})
-			.then(res => {
-				dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
-			})
-			.catch(e => {
-				if (axios.isCancel(e)) return;
-				dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
-			});
-		const cancelToken2 = axios.CancelToken.source();
-		axios
-			.get(baseURL, {
-				cancelToken: cancelToken2.token,
-				params: { markdown: true, page: page + 1, ...params },
-			})
-			.then(res => {
-				dispatch({
-					type: ACTIONS.UPDATE_NEXT_PAGE,
-					payload: { hasNextPage: res.data.length !== 0 },
+	const [ state, dispatch ] = useReducer(reducer, { jobs: [], loading: true });
+	useEffect(
+		() => {
+			const cancelToken1 = axios.CancelToken.source();
+			dispatch({ type: ACTIONS.MAKE_REQUEST });
+			axios
+				.get(baseURL, {
+					cancelToken: cancelToken1.token,
+					params: { markdown: true, page: page, ...params }
+				})
+				.then((res) => {
+					dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+				})
+				.catch((e) => {
+					if (axios.isCancel(e)) return;
+					dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
 				});
-			})
-			.catch(e => {
-				if (axios.isCancel(e)) return;
-				dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
-			});
+			const cancelToken2 = axios.CancelToken.source();
+			axios
+				.get(baseURL, {
+					cancelToken: cancelToken2.token,
+					params: { markdown: true, page: page + 1, ...params }
+				})
+				.then((res) => {
+					dispatch({
+						type: ACTIONS.UPDATE_NEXT_PAGE,
+						payload: { hasNextPage: res.data.length !== 0 }
+					});
+				})
+				.catch((e) => {
+					if (axios.isCancel(e)) return;
+					dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+				});
 
-		return () => {
-			cancelToken1.cancel();
-			cancelToken2.cancel();
-		};
-	}, [params, page]);
+			return () => {
+				cancelToken1.cancel();
+				cancelToken2.cancel();
+			};
+		},
+		[ params, page ]
+	);
 	return state;
 }
